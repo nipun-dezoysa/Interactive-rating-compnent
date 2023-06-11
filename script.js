0d9386a1a2913e528fef52b6ceb99986ae42647e
Nipun Avishka De Zoysa <nipunavishka123@gmail.com>
2023-06-11 17:03:57 +0530

    update

--- script.js
+++ script.js
@@ -20,8 +20,39 @@ function oii(b){
     a=b;
 }
 function sub(){
+    const box = document.getElementById('box');
     const mains = document.getElementById('main');
     mains.remove();
-    const box = document.getElementById('box');
-    box.innerHTML = "gammac";
+
+    const thank = document.createElement('div');
+    const thankc = document.createAttribute('class');
+    thankc.value = 'thank';
+    thank.setAttribute(thankc);
+
+    const img = document.createAttribute('img');
+    const imgsrc = document.createAttribute('src');
+    imgsrc.value= 'images/illustration-thank-you.svg';
+    img.setAttribute(imgsrc);
+
+    const rating = document.createElement('div');
+    const ratingc = document.createAttribute('class');
+    const ratingcon = document.createTextNode('You selected 4 out of 5');
+    rating.appendChild(ratingcon);
+    ratingc.value = 'lb';
+    rating.setAttribute(ratingc);
+
+    const hed = document.createElement('h1');
+    const hedcon = document.createTextNode('Thank you!');
+    hed.appendChild(hedcon);
+
+    const des = document.createElement('p');
+    const descon = document.createTextNode('We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!');
+    des.appendChild(descon);
+
+    thank.appendChild(img);
+    thank.appendChild(rating);
+    thank.appendChild(hed);
+    thank.appendChild(des);
+    document.querySelector('#box').appendChild(thank);
+
 }
